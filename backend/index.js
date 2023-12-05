4269a04b8042d72a92067820efd7cd054ee7e995
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-12-05 23:58:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,7 @@ mongoose.connect('mongodb+srv://anirudhkala110:6RL4PMGvvOIxGDSt@cluster0.kypwz4x
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: ["https://legal-brothers-online.vercel.app"],
-        // origin: ["http://localhost:3024"],
+        origin: ["https://legal-brothers-online.vercel.app/"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
