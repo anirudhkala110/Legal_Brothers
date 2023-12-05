f492b1d56a0f1911a5d1645f6feded1bcd7adac0
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-12-05 23:37:29 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,10 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://anirudhkala110:6RL4PMGvvOIxGDSt@cluster0.kypwz4x.mongodb.net/Legal_Brothers?retryWrites=true&w=majority')
-
-app.get('/', (req, res) => {
-  console.log("Connected");
-});
+// , {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 app.post('/api/saveData', async (req, res) => {
   const formData = req.body;
