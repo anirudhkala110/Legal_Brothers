829de110dadbd8f684a1a055da6a0bf37ad7b063
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-12-06 12:34:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,40 +10,43 @@ const app = express();
 const port = 8096 || process.env.PORT;
 
 mongoose.connect('mongodb+srv://anirudhkala110:6RL4PMGvvOIxGDSt@cluster0.kypwz4x.mongodb.net/Legal_Brothers?retryWrites=true&w=majority')
-
+// mongoose.connect('mongodb://127.0.0.1:27017/Legal_Brothers')
+app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: ["https://legal-brothers-online.vercel.app"],
+        // origin: ["https://heliverse-1ugi.vercel.app"],
+        origin: [""],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
 ))
-app.options('/api/saveData', cors());
-
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    console.log("COnnected")
+    console.log("Connected")
     return res.json('Connected')
 })
 
 
 app.post('/api/saveData', async (req, res) => {
-    const formData = req.body;
-
+    const name = req.body.name;
+    const email = req.body.email;
+    const query = req.body.query;
+    const mobile = req.body.mobile;
+    const date = req.body.date;
+    const time = req.body.time;
+    const id = req.body.id
+    console.log(name, email, mobile, query, date, time, id)
     try {
         // Create a new Contact document using the Contact model
         const newContact = new Contact({
-            name: formData.name,
-            mobile: formData.mobile,
-            email: formData.email,
-            query: formData.query,
-            // Optionally, date and time will be automatically set to current date/time if not provided
-            date: formData.date,
-            time: formData.time,
+            name: name,
+            mobile: mobile,
+            email: email,
+            query: query,
+            date: date,
+            time: time,
         });
-        if(name == '' || mobile == "" || email == "" || query=="" || !name || !mobile || !email || !query)
-            return res.json({success:false}}
+
         // Save the new contact data to MongoDB
         await newContact.save();
 
